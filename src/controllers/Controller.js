57b9d04994f97bbcc9c1131ac7aaca9631f764f9
João Pedro Santana <jpsantana003@gmail.com>
2025-03-19 11:29:33 -0300

    Feat: Apí

---
+++ src/controllers/Controller.js
@@ -0,0 +1,17 @@
+class Controller {
+  constructor (entidadeService) {
+    this.entidadeService = entidadeService;
+  }
+
+  async pegaTodos(req, res) {
+    try {
+      const listaDeRegistro = await this.entidadeService.pegaTodosOsRegistros();
+      return res.status(200).json(listaDeRegistro);
+    } catch (erro) {
+      return res.status(500).json(erro.message);
+    }
+    
+  }
+}
+
+module.exports = Controller;
